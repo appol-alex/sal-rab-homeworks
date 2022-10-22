1592d9810fbf7ec591d3d5ba8aa7da9bb3305bc6
Polina K <appol.alex@gmail.com>
2022-10-22 22:24:38 +0300

    Final task done by appol_alex

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    
+    let data = {client: `${name} ${phone}`, order: {address: "", sum}, goods: []};
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let temporary = {title: goods[i].title, count: goods[i].count};
+        data.goods.push(temporary);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+     let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
+sendRequest()
